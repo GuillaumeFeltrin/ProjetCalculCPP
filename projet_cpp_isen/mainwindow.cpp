cbf06920eed1137d37d4eda6b81f6c7eb7a1b3b8
Guillaume <guillaume.feltrin@gmail.com>
2020-05-29 00:04:51 +0200

    Add v3 IHM

--- projet_cpp_isen/mainwindow.cpp
+++ projet_cpp_isen/mainwindow.cpp
@@ -14,14 +14,24 @@ MainWindow::MainWindow() : QWidget()  // spécification du constructeur
     showVal->setToolTip("Afficher la valeur");
     simplify->setToolTip("Simplifier l'expression");
 
-
+    // page 1
     vbox1->addWidget(saisie);
     vbox1->addWidget(classique);
     vbox1->addWidget(polo);
     vbox1->addWidget(showVal);
     vbox1->addWidget(simplify);
-
     page1->setLayout(vbox1);
+
+
+    // page 2
+    vbox2->addWidget(deud);
+    vbox2->addWidget(troid);
+    page2->setLayout(vbox2);
+
+    // page 2
+    vbox3->addWidget(acces);
+    vbox3->addWidget(sauv);
+    page3->setLayout(vbox3);
 
 
 
@@ -42,6 +52,14 @@ MainWindow::MainWindow() : QWidget()  // spécification du constructeur
     QObject::connect(simplify, SIGNAL(clicked()), this, SLOT(ouvrirFonctionSimplify()));
     QObject::connect(classique, SIGNAL(clicked()), this, SLOT(ouvrirExpClassique()));
     QObject::connect(polo, SIGNAL(clicked()), this, SLOT(ouvrirExpPolo()));
+
+    // page 2
+    QObject::connect(deud, SIGNAL(clicked()), this, SLOT(ouvrirGraph2D()));
+    QObject::connect(troid, SIGNAL(clicked()), this, SLOT(ouvrirGraph3D()));
+
+    // page 3
+    QObject::connect(acces, SIGNAL(clicked()), this, SLOT(ouvrirSauvegarde()));
+    QObject::connect(sauv, SIGNAL(clicked()), this, SLOT(ouvrirEnregistrerSauvegarde()));
 
     fenetre.show();
 }
@@ -89,6 +107,25 @@ void MainWindow::ouvrirFonctionShowVal()
     QMessageBox::information(this, "Valeur", "Valeur");
 }
 
+void MainWindow::ouvrirGraph2D()
+{
+    QMessageBox::information(this, "2D", "2D");
+}
 
+void MainWindow::ouvrirGraph3D()
+{
+    QMessageBox::information(this, "3D", "3D");
+}
+
+
+void MainWindow::ouvrirSauvegarde()
+{
+    QMessageBox::information(this, "Sauvegarde", "Sauvegarde");
+}
+
+void MainWindow::ouvrirEnregistrerSauvegarde()
+{
+    QMessageBox::information(this, "Sauvegarde", "Sauvegarde");
+}
 
 
